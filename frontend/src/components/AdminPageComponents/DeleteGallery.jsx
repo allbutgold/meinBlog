910f660a5992015bf69a928831b9d4f2614a5880
allbutgold <contact@leokieffer.com>
2023-08-02 22:50:43 +0200

    fix deleteGallery endpoint

--- frontend/src/components/AdminPageComponents/DeleteGallery.jsx
+++ frontend/src/components/AdminPageComponents/DeleteGallery.jsx
@@ -1,61 +1,63 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 function DeleteGallery() {
   const [objects, setObjects] = useState([])
-  const [selectedObject, setSelectedObject] = useState('')
-  const [error, setError] = useState('');
+  const [selectedObject, setSelectedObject] = useState("")
+  const [error, setError] = useState("")
 
   const API_URL = import.meta.env.VITE_BACKEND_URL
 
   useEffect(() => {
-    fetch('http://localhost:9999/api/v1/getGalleries')
-      .then(res => res.json())
-      .then(data => setObjects(data))
+    fetch("http://localhost:9999/api/v1/getGalleries")
+      .then((res) => res.json())
+      .then((data) => setObjects(data))
   }, [])
 
   const handleDelete = () => {
     fetch(`${API_URL}api/v1/deleteGallery/${selectedObject}`, {
-      method: 'DELETE'
+      credentials: "include",
+      method: "DELETE",
     })
-      .then(res => {
+      .then((res) => {
         if (res.status === 401) {
-          setError('You are not authorized to delete galleries')
-          
+          setError("You are not authorized to delete galleries")
         } else if (!res.ok) {
-          setError('failed to delete gallery')
-        } return res.json()
+          setError("failed to delete gallery")
+        }
+        return res.json()
       })
-      .then(data => {
-        const newObjects = objects.filter(item => item._id !== selectedObject._id)
-        
+      .then((data) => {
+        const newObjects = objects.filter(
+          (item) => item._id !== selectedObject._id
+        )
+
         setObjects(newObjects)
-        setSelectedObject('')
+        setSelectedObject("")
       })
-      .catch(err => console.error(err))
+      .catch((err) => console.error(err))
   }
 
   const handleSelect = (e) => {
     setSelectedObject(e.target.value)
-    
   }
 
   return (
     <div>
       <h3>delete existing gallery</h3>
-      {error && <p style={{color: 'red'}}>{error}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <select value={selectedObject} id={objects} onChange={handleSelect}>
-        <option value="">select gallery</option>
-        {objects.map(item => (
+        <option value=''>select gallery</option>
+        {objects.map((item) => (
+          <option key={item._id} value={item._id}>
+            {item.title}
+          </option>
+        ))}
+      </select>
+      <br />
 
-
-            <option key={item._id} value={item._id}>{item.title}</option>
-
-          ))}
-      </select><br/>
-      
       <button onClick={handleDelete}>delete</button>
     </div>
   )
 }
 
-export default DeleteGallery;
+export default DeleteGallery
